e281043905155408ac6da830e7d97e19dd367eb0
WoohyunCha <woohyun321@snu.ac.kr>
2025-04-24 11:58:23 +0900

    added DOFs in the upper arm. Now we use the shoulder roll as well

--- include/cc.h
+++ include/cc.h
@@ -39,10 +39,10 @@ public:
 
     Eigen::Vector3d mat2euler(Eigen::Matrix3d mat);
 
-    static const int num_action = 18;
+    static const int num_action = 20;
     static const int num_actuator_action = 12;
-    static const int num_cur_state = 84;
-    static const int num_cur_internal_state = 84;
+    static const int num_cur_state = 90;
+    static const int num_cur_internal_state = 90;
     static const int num_state_skip = 2;
     static const int num_state_hist = 10;
     static const int num_state = num_cur_state * num_state_hist; // num_cur_internal_state*num_state_hist+num_action*(num_state_hist-1);
